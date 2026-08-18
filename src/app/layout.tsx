5ba51152b97a2fa26b1da573857546b73010d130
em3rc0dTh <eduardo.farid@thradex.com>
2026-08-17 22:07:48 -0500

    style: load visual material v2 layer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,7 @@ import "./public-surfaces.css";
 import "./public-surfaces-fixes.css";
 import "./accessibility.css";
 import "./accessibility-fixes.css";
+import "./visual-material-v2.css";
 
 const instrumentSans = Instrument_Sans({
   subsets: ["latin"],
