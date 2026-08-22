bc355213a089c8fafd4b682bbd4ee2399bb797e0
em3rc0dTh <eduardo.farid@thradex.com>
2026-08-22 13:21:53 -0500

    perf: load About visual styles only on About

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { SiteHeader } from "@/components/shell/site-header";
+import "../visual-acceptance-v2b.css";
 
 export const metadata = {
   title: "About",
